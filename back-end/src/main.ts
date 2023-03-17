4377ebcf961abef6211c7a8530b4b1b5e02a39fd
saagas-code <saagas.code@gmail.com>
2023-03-17 11:13:04 -0300

    deleted swagger

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { join } from 'path';
-import { SwaggerModule } from '@nestjs/swagger';
-import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,11 +9,6 @@ async function bootstrap() {
     whitelist: true,
     transform: true
   }))
-
-  // const document = JSON.parse(
-  //   (await readFile(join(process.cwd(), './src/swagger.json'))).toString('utf-8')
-  // )
-  // SwaggerModule.setup('/api', app, document);
 
   app.enableCors();
   await app.listen(process.env.APP_PORT || 3000);
